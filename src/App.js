2d7e1ebe3fea60e47edad408d08e9c4c76740dd8
anand-yadav55 <anand55yadav@gmail.com>
2021-09-05 12:38:39 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import {Container,
+    Grid,
+    Paper,
+    Box,
+    Card,
+    CardContent,
+    Typography} from '@material-ui/core';
 
-function App() {
+function App(props) {
+  const data = props.data;
+  console.log(data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth='md' pt={4} m={3}>
+
+      <Card variant='outlined'>
+        <CardContent>
+          <Typography variant='h5' component='h2'>
+            {data.formName}
+          </Typography>
+          <Typography variant='body2' component='p'>
+            {data.formDesc}
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card>
+        <Grid container spacing={2}>
+          {data.data.map((item, idx)=>{
+            let rules;
+            if(item.fieldrules){
+              if(item.fieldrules.length!==0){
+                rules = item.fieldrules[0];
+                console.log(rules)
+              }
+            }
+            return <Grid key={idx} item xs={12}>
+              <Paper elevation={2}>
+                
+                  <Box>
+ 
+                    <label htmlFor={item.fieldname}>{item.fieldlabel}</label>
+                    
+                    switch({item.fieldname}){
+                      case "text":{
+                        
+                      }
+                    }
+                    {(item.fieldtype==='select')?
+                      <select name={item.fieldname}
+                        placeholder={item.placeholder}
+                        required={
+                          (rules)?
+                              (rules.required?true:false)
+                              :
+                              false
+                        }  
+                      >
+                        {
+                          
+                          item.options.map((item, idx)=>{
+                            return <option key={idx} value={item}>{item}</option>
+                          })
+                          
+                        }
+                      </select>
+                        :
+                        <input 
+                          name={item.fieldname} 
+                          placeholder={item.fieldplaceholder} 
+                          required={
+                            (rules)?
+                              (rules.required?true:false)
+                              :
+                              false
+                          } 
+                          type={item.fieldtype}
+                        />
+                    }
+                  </Box>
+                
+              </Paper>
+            </Grid>
+          })}
+        </Grid>
+      </Card>
+    </Container>
   );
 }
 
